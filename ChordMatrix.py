8b9b7d036d1675262446928cd9916ca578989fdd
ruslanhalitov <ruslan2439@gmail.com>
2021-03-01 13:28:08 +0100

    Fixed a chord generation

--- ChordMatrix.py
+++ ChordMatrix.py
@@ -6,11 +6,15 @@ import numpy as np
 
 
 def chord_mask(N, base=2):
-    ts = int(round(np.log(N)/np.log(base)))
+    ts = int(round(np.log(N)/np.log(base))) + 1
     ch = np.eye(N)
     for i in range(N):
         for t in range(ts):
-            ch[i, abs((i + 2 ** t)) - N] = 1
+            # ch[i, abs((i + base ** t)) % N] = 1
+            ch[i, ((i - 1) + base ** t) % N] = 1
     return ch
+
+print(chord_mask(11, 2))
+print(chord_mask(17, 2))
 
 
