8b0ec2b21f7fd0f1a3a459da43e04e98e898939c
ruslanhalitov <ruslan2439@gmail.com>
2021-03-19 17:03:18 +0300

    Added test environment (not full)

--- SmallTestEnvironment.py
+++ SmallTestEnvironment.py
@@ -5,8 +5,9 @@ Testing environment
 """
 
 # Imports
-import numpy as np
+import sys
 import math
+import numpy as np
 from ChordMatrix import chord_mask
 
 # Globals
@@ -19,15 +20,44 @@ parameters = {
 LRg = 0.01
 LRf = 0.01
 
+
+# Activation functions and its derivatives
 def relu(x):
     return max(0, x)
+
 
 def leaky_relu(x, slope=0.01):
     return max(slope * x, x)
 
+
+def tanh(x):
+    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def d_relu(x):
+    return 1 if x > 0 else 0
+
+
+def d_leaky_relu(x, slope=0.01):
+    return 1 if x > 0 else slope
+
+
+def d_tanh(x):
+    return 1 - tanh(x) ** 2
+
+
+def d_sigmoid(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
 activation_g = relu
 activation_f = relu
-
+d_activation_g = d_relu
+d_activation_f = d_relu
 
 
 def get_X(N, embed_size):
@@ -51,12 +81,12 @@ def get_Vgt(X, transform='exp'):
         'exp': np.exp,
         'log': np.log
     }
-    assert transform in transform_to_func.keys(), f"Unknown transformation {transform}," \
-                                                  f" available: {transform_to_func.keys()}"
+    assert transform in transform_to_func.keys(
+    ), f"Unknown transformation {transform}," f" available: {transform_to_func.keys()}"
     return transform_to_func[transform](X)
 
 
-def init_f(N, d):
+def init_f():
     """
     Initializes f (d -> N)
     :param N: Sequence length
@@ -65,7 +95,7 @@ def init_f(N, d):
     """
     weights = np.zeros((N, d))
     bias = np.zeros(N)
-    grad_weights = np.zeros((N, N, d))
+    grad_weights = np.zeros((N * d, N, N))
     grad_bias = np.zeros(N, N)
 
     parameters['f']['weights'] = weights
@@ -74,7 +104,7 @@ def init_f(N, d):
     parameters['f']['grad_bias'] = grad_bias
 
 
-def init_g(N, d):
+def init_g():
     """
     Initializes g (d -> d)
     :param N: Sequence length
@@ -83,7 +113,7 @@ def init_g(N, d):
     """
     weights = np.zeros((d, d))
     bias = np.zeros(d)
-    grad_weights = np.zeros((N, d, d))
+    grad_weights = np.zeros((d * d, N, d))
     grad_bias = np.zeros(N, d)
 
     parameters['g']['weights'] = weights
@@ -106,13 +136,14 @@ def calculate_loss(V_gt, V0):
 
 def forward(X, parameters):
     """
-    Forward pass X -> V0
+    Forward pass X -> V0. Also computes d_activation for
     :param parameters: Current parameters of f and g
-    :return: V0
+    :return: V0, V, W, d_activation values for the backward pass
     """
 
     # V
     V = np.zeros(N, d)
+    d_activation_values_g = np.zeros(N, d)
     assert np.shape(V) == np.shape(X), "V and X have different size"
     xi = parameters['g']['weights']
     bias = parameters['g']['bias']
@@ -122,10 +153,13 @@ def forward(X, parameters):
         # columns
         for j in range(d):
             # bias[j] + X[i, 0] * xi[j, 0] + X[i, 1] * xi[j, 1]
-            V[i, j] = activation_g(bias[j] + np.sum([X[i, _] * xi[j, _] for _ in range(d)]))
+            value = bias[j] + np.sum([X[i, _] * xi[j, _] for _ in range(d)])
+            V[i, j] = activation_g(value)
+            d_activation_values_g[i, j] = d_activation_g(value)
 
     # W
     W_unmask = np.zeros(N, N)
+    d_activation_values_f = np.zeros(N, N)
     theta = parameters['f']['weights']
     bias = parameters['f']['bias']
 
@@ -134,40 +168,52 @@ def forward(X, parameters):
         # columns
         for j in range(N):
             # bias[j] + X[i, 0] * theta[j, 0] + X[i, 1] * theta[j, 1] + X[i, 2] * theta[j, 2]
-            W_unmask[i, j] = activation_f(bias[j] + np.sum([X[i, _] * theta[j, _] for _ in range(N)]))
+            value = bias[j] + np.sum([X[i, _] * theta[j, _] for _ in range(N)])
+            W_unmask[i, j] = activation_f(value)
+            d_activation_values_f[i, j] = d_activation_f(value)
 
-    # masking W (elementwise)
+    # masking W (elementwise product)
     W = W_unmask * chord_mask(N)
+    d_activation_values_f = d_activation_values_f * chord_mask(N)
 
-    # WV
+    # WV (matrix multiplication)
     V0 = np.matmul(W, V)
 
-    return V0
+    return V0, V, W, d_activation_values_g, d_activation_values_f
+
+
+def backward(parameters, V0, V, W, d_activation_values_g, d_activation_values_f, V_gt):
+    """
+    Calculates gradients of the Loss function (J) w.r.t (theta, bias) and (xi, bias)
+    and stores it in parameters['grad_weights'] and parameters['grad_bias']
+    for f and g respectively.
+
+    :param parameters: parameters of the model, gradient containers
+    :param V0: final result of the forward pass
+    :param V: tensor of size (Nxd)
+    :param W: tensor of size (NxN), after masking
+    :param d_activation_values_g: tensor of size (Nxd)
+    :param d_activation_values_f: tensor of size (NxN)
+    :param V_gt: tensor of size (Nxd), ground truth
+    :return: None
+    """
+
+    # TODO: add biases
+
+    dj_dv0 = -2 * (V_gt - V0).T               # (d, N)
+    dv0_dw = V                                # (N, d)
+    dv0_dv = W                                # (N, N)
+    dj_dw = dj_dv0 * dv0_dw                   # (d, d)
+    dj_dv = dj_dv0 * dv0_dv                   # (d, N)
+
+    # g
+
+
+    # f
+
 
 # ____________________________________________________
 # END OF NEW
-
-
-def backward(parameters, N, V, memory):
-    """
-    #calculates gradients of J respect to V and F
-    :return:
-    """
-    grad_values = {}
-    L = int(math.log(N, 2))
-    K = 2
-    grad_values['dv'] = 2 * np.matmul(1 / memory['W' + str(L)].T, parameters['V'] - V)
-    for i in range(L):
-        df = np.zeros((N, 2))
-        for j in range(N):
-            for k in range(K):
-                tmp1 = 2 * np.matmul(1 / memory['W' + str(L - i)].T, parameters['V' + str(L - i)] - V)
-                tmp2 = np.matrix(parameters['V' + str(L - i)][(j + np.power(2, k)) % N]).T
-                tmp = np.matmul(tmp1, tmp2)
-                df[j][k] = tmp[j][0]
-        grad_values['dF' + str(L - i)] = df
-
-    return grad_values
 
 
 def update_parameters(parameters, grad_values, learning_rate, N):
@@ -181,13 +227,14 @@ def update_parameters(parameters, grad_values, learning_rate, N):
     L = int(math.log(N, 2))
     parameters['V'] -= learning_rate * grad_values["dv"]
     for i in range(L):
-        parameters['F' + str(L - i)] -= learning_rate * grad_values["dF" + str(L - i)]
+        parameters['F' + str(L - i)] -= learning_rate * \
+            grad_values["dF" + str(L - i)]
         parameters['V' + str(L - i)] -= learning_rate * grad_values["dv"]
 
     return parameters
 
 
-def traning(epochs, learning_rate, N):
+def training(epochs, learning_rate, N):
     """
     normal training process as in NN
     :param epochs:
@@ -195,13 +242,13 @@ def traning(epochs, learning_rate, N):
     :param N:
     :return:
     """
-    parameters = init_layers(N)
     cost_history = []
     for i in range(epochs):
         V, memory = forward(parameters)
-        cost = get_cost_value(V, parameters['V_copy'])
+        cost = calculate_loss(V, parameters['V_copy'])
         grad_values = backward(parameters, N, V, memory)
-        parameters = update_parameters(parameters, grad_values, learning_rate, N)
+        parameters = update_parameters(
+            parameters, grad_values, learning_rate, N)
         cost_history.append(cost)
         print(cost)
         # print(V)
@@ -210,4 +257,4 @@ def traning(epochs, learning_rate, N):
 
 
 if __name__ == "__main__":
-    traning(2, 0.01, 16)
+    training(2, 0.01, 16)
