8b619ea421ceb5c1cb586dbcb3c5f346b6cf0861
wiedersehne <wiedersehne@gmail.com>
2021-04-07 20:14:43 +0200

    Update SMF_torch_extension.py

--- SMF_torch_extension.py
+++ SMF_torch_extension.py
@@ -3,17 +3,21 @@ import numpy as np
 
 
 class SMFNet(torch.nn.Module):
-    def __init__(self, D_in, H1, H2, n_layer):
+    def __init__(self, D_in, H1, H2, n_layer, depth):
         """
         :param D_in: Embedded size of original data
         :param H1: Hidden layer dimension of g
         :param H2: Hidden layer dimension of f
+        :param n_layer: Layer number of SMFNet
+        :param depth: layer number of f and g
         """
         super(SMFNet, self).__init__()
         self.f_array = []
+        self.g_linears = torch.nn.ModuleList(torch.nn.Linear(D_in, H1) for d in range(depth))
         for m in range(n_layer):
-            self.f_array.append(torch.nn.Linear(D_in, H2))
-        self.g_linear = torch.nn.Linear(D_in, H1)
+            f_linears = torch.nn.ModuleList(torch.nn.Linear(D_in, D_in) for d in range(depth-1))
+            f_linears.append(torch.nn.Linear(D_in, H2))
+            self.f_array.append(f_linears)
         self.relu = torch.nn.ReLU()
 
     def make_chord(self, N):
@@ -28,7 +32,7 @@ class SMFNet(torch.nn.Module):
 
         return chord_mask
 
-    def forward(self, X, n_layer):
+    def forward(self, X, n_layer, depth):
         """
         :param X: Nxd data
         :return: V(M-1) M is the layer number.
@@ -36,10 +40,14 @@ class SMFNet(torch.nn.Module):
         N = X.shape[0]
         W = [torch.zeros(N, N)]*n_layer
         chord_mask = self.make_chord(N)
-        V0 = self.relu(self.g_linear(X.float()))
         #print(V0)
+        V0 = X.float()
+        for d in range(depth):
+            V0 = self.relu(self.g_linears[d](V0))
         for m in range(n_layer):
-            F = self.relu(self.f_array[m](X.float()))
+            F = X.float()
+            for d in range(depth):
+                F = self.relu(self.f_array[m][d](F))
             W[m] = F*chord_mask
             V0 = torch.matmul(W[m], V0)
 
