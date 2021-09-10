1b0c155c0597d4a877ac5957d34dbe2a39258f31
Ruslan Khalitov <ruslan2439@gmail.com>
2021-09-11 00:50:34 +0200

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-nn_cfg = {
-    'dataset': ["exp", "ln", "permute", "iris"],
-    'masking': ["chord", "no_chord"],
-    'optimizer': ["Adam", "SGD", "RMRProp"],
-    'LR': ["0.1", "0.01", "1e-3", "1e-4", "1e-5", "1e-6", "1e-7"],
-}
