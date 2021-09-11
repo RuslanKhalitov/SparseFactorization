80060215e010481ace0befc00729197045f68419
ruslanhalitov <ruslan2439@gmail.com>
2021-09-11 20:56:57 +0200

    updated inference

--- LRA/attention_maps/pathfinder_inference.py
+++ LRA/attention_maps/pathfinder_inference.py
@@ -19,7 +19,8 @@ cfg_training = config['pathfinder']['training']
 
 # Setting device
 print(torch.cuda.get_device_name(cfg_training["device_id"]))
-torch.cuda.set_device(cfg_training["device_id"])
+# torch.cuda.set_device(cfg_training["device_id"])
+torch.cuda.set_device(0)
 
 # Make an instance for extracting the attention map
 class ChangedPSF(PSFNet):
@@ -114,7 +115,7 @@ testset = DatasetCreator(
 
 testloader = torch_geometric.data.DataLoader(
     testset,
-    batch_size=cfg_training['batch_size'],
+    batch_size=8,
     shuffle=False,
     drop_last=True,
     num_workers=2
@@ -123,8 +124,9 @@ testloader = torch_geometric.data.DataLoader(
 
 import matplotlib.pyplot as plt
 import numpy as np
-import cv2
-from skimage import exposure
+import os
+if not os.path.exists('att_matr_path'):
+    os.makedirs('att_matr_path')
 
 def take_ind_around(central_ind):
     central_ind = int(central_ind)
@@ -154,33 +156,24 @@ for i, (X, Y) in tqdm(enumerate(testloader), total=len(testloader)):
     correct += predicted.eq(Y).sum().item()
 
     # att map visualization
-    # tran = transforms.Compose([transforms.Normalize(0.5, 0.5), transforms.ToPILImage()])
-    for im_index in range(cfg_training['batch_size']):
-        # print(torch.matrix_rank(att_map[im_index]))
-        # sys.exit()
-        img_path = df.iloc[i * cfg_training['batch_size'] + im_index][0]
+    for im_index in range(8):
+        img_path = df.iloc[i * 8 + im_index][0]
         fold, name = img_path.split('/')[-2:]
-        # print(fold, name)
-        if str(fold) == '107' and name == 'sample_818.png':
-            print('saving pdf')
-            sorted, indices = torch.topk(X[im_index], 2)
-            indices_full = take_ind_around(indices[0]) + take_ind_around(indices[1])
-            ddf = att_map[im_index].cpu().T.reshape((1024, 32, 32))[indices_full].sum(0)
-            ddf = ddf.detach().numpy()
-            ddf = ddf - ddf.min()
-            ddf = ddf.clip(np.quantile(ddf, 0.7), np.quantile(ddf, 1.0)) ** .5
-            img = plt.imread(img_path)
+        sorted, indices = torch.topk(X[im_index], 2)
+        indices_full = take_ind_around(indices[0]) + take_ind_around(indices[1])
+        ddf = att_map[im_index].cpu().T.reshape((1024, 32, 32))[indices_full].sum(0)
+        ddf = ddf.detach().numpy()
+        ddf = ddf - ddf.min()
+        ddf = ddf.clip(np.quantile(ddf, 0.7), np.quantile(ddf, 1.0)) ** .5
+        img = plt.imread(img_path)
 
-            # figure, axs = plt.subplots(1, 2)
-            plt.imshow(img, cmap='gray')
-            plt.axis('off')
-            plt.savefig(f'att_matr_path/1{fold}_{name[:-4]}.pdf', bbox_inches='tight', pad_inches=0)
-            # axs[0].set_title('Original image')
-            plt.imshow(ddf, cmap=plt.get_cmap('inferno'))
-            plt.axis('off')
-            plt.colorbar()
-            plt.savefig(f'att_matr_path/2{fold}_{name[:-4]}.pdf', bbox_inches='tight', pad_inches=0)
-            # axs[1].set_title('Att Map')
+        figure, axs = plt.subplots(1, 2, gridspec_kw = {'wspace':0.05, 'hspace':0.05})
+        axs[0].imshow(img, cmap='gray')
+        axs[0].axis('off')
+        axs[1].imshow(ddf, cmap=plt.get_cmap('inferno'))
+        axs[1].axis('off')
+        plt.savefig(f'att_matr_path/{fold}_{name[:-4]}.png', bbox_inches='tight', pad_inches=0)
+        plt.close()
 
 print("Test accuracy: {}".format(100.*correct/total))
 accuracy = int(100.*correct/total)
